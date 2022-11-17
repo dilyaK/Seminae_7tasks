19faaaa77c40e6914730b6aedc930f2c9af69174
dilya <1dilnora777@gmail.com>
2022-11-17 14:20:06 +0500

    task1 added

---
+++ task001_47/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Enter number of M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number of N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+        
+Random r = new Random();
+        
+float[,] arr = new float[m, n];
+
+for (int i = 0; i < m; ++i)
+{
+	for (int j = 0; j < n; ++j)
+        {
+                arr[i, j] = (float)r.NextDouble() * 20 - 10;
+        }
+}
+
+for (int i = 0; i < m; ++i)
+{
+	for (int j = 0; j < n; ++j)
+        {
+                Console.Write(string.Format("{0,7:F3}", arr[i, j]) + "\t"); 
+        }
+        Console.WriteLine();
+}
