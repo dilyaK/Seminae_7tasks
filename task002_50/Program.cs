307599561c3644257aeeceb6bfa4c6e6dd20f476
dilya <1dilnora777@gmail.com>
2022-11-18 23:13:16 +0500

    task3 added

---
+++ task002_50/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.Write("Enter M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+        
+Random r = new Random();
+        
+float[,] arr = new float[m, n];
+
+for (int i = 0; i < m; ++i)
+{
+	for (int j = 0; j < n; ++j)
+        {
+                arr[i, j] = (float)r.NextDouble() * 20 - 10;
+        }
+}
+
+for (int i = 0; i < m; ++i)
+{
+	for (int j = 0; j < n; ++j)
+        {
+                Console.Write(string.Format("{0,7:F3}", arr[i, j]) + "\t"); // {номер аргумента,количество позиций на аргумент:<Спецификатор><Число знаков после запятой>}
+        }
+        Console.WriteLine();
+}
+
+Console.Write("Enter the row number: ");
+int row_pos = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the column number: ");
+int col_pos = Convert.ToInt32(Console.ReadLine());
+        
+if (row_pos >= 0 && row_pos < m && col_pos >= 0 && col_pos < n)
+{
+        Console.WriteLine("Number at this positon = " + arr[row_pos, col_pos]);
+}
+else
+{
+        Console.WriteLine("No such number in the array");
+}
---
+++ task003_52/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Write("Enter M(number of columns): ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter  N(number of rows): ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Random r = new Random();
+
+int[,] arr = new int[m, n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; ++j)
+    {
+        arr[i, j] = r.Next(0, 11); 
+        Console.Write(arr[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+
+
+Console.WriteLine("Column mean:");
+float[] average_by_cols = new float[n];
+
+for (int j = 0; j < n; ++j) // итерируемся по столбцам
+{
+    long current_sum = 0;
+    for (int i = 0; i < m; ++i) // итерируемся по строкам
+    {
+        current_sum += arr[i, j];
+    }
+    average_by_cols[j] = (float)current_sum / m;
+    Console.Write(string.Format("{0:F2}", average_by_cols[j]) + "\t");
+}
